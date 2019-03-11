dedf13a34cb64872103ed95c01a18fa747d20a1e
Gábor Bazsó <bazsogabor0911@gmail.com>
2019-03-11 21:08:37 +0100

    Turn Signals

--- src/main/java/hu/oe/nik/szfmv/automatedcar/visualization/Dashboard.java
+++ src/main/java/hu/oe/nik/szfmv/automatedcar/visualization/Dashboard.java
@@ -14,6 +14,9 @@ public class Dashboard extends JPanel {
 
     Gui parent;
 
+    private TurnSignal leftTurnSignal;
+    private TurnSignal rightTurnSignal;
+
     private Thread timer = new Thread() {
         int difference;
 
@@ -40,7 +43,52 @@ public class Dashboard extends JPanel {
 
         parent = pt;
 
+        leftTurnSignal = addTurnSignal(new Point(30, 150), false);
+        rightTurnSignal = addTurnSignal(new Point(180, 150), true);
+
         timer.start();
     }
 
+    private TurnSignal addTurnSignal(Point position, boolean isRightSignal) {
+        TurnSignal turnSignal = new TurnSignal(position, isRightSignal);
+        add(turnSignal);
+        return turnSignal;
+    }
+
+    public class TurnSignal extends JPanel {
+        private final int WIDTH=30;
+        private final int HEIGHT=30;
+
+        Color color;
+        Point position;
+        boolean isRightSignal;
+
+        public void setColor(Color color) {
+            this.color = color;
+        }
+
+        protected void paintComponent(Graphics g) {
+            super.paintComponent(g);
+            g.setColor(new Color(0x888888));
+            g.fillOval(-10, -10, 50, 50);
+            g.setColor(color);
+            if (isRightSignal) {
+                int[] x = new int[] { 0, 10, 10, 20, 10, 10, 0 };
+                int[] y = new int[] { 10, 10, 0, 14, 28, 18, 18 };
+                g.fillPolygon(x, y, 7);
+            } else {
+                int[] x = new int[] { 0, 10, 10, 20, 20, 10, 10 };
+                int[] y = new int[] { 14, 0, 10, 10, 18, 18, 28 };
+                g.fillPolygon(x, y, 7);
+            }
+        }
+
+        public TurnSignal(Point position, boolean isRightArrow)
+        {
+            this.position=position;
+            this.isRightSignal =isRightArrow;
+            this.color=Color.black;
+            this.setBounds(position.x, position.y, WIDTH, HEIGHT);
+        }
+    }
 }
