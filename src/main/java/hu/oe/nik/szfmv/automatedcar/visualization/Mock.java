158fa5de762323958e9ed4570b22a67481847c94
pintergreg <pintergreg@gmail.com>
2019-03-28 08:16:54 +0100

    Reformat code

--- src/main/java/hu/oe/nik/szfmv/automatedcar/visualization/Mock.java
+++ src/main/java/hu/oe/nik/szfmv/automatedcar/visualization/Mock.java
@@ -1,11 +1,30 @@
 package hu.oe.nik.szfmv.automatedcar.visualization;
 
 import hu.oe.nik.szfmv.automatedcar.model.WorldObject;
+
 import java.util.ArrayList;
 import java.util.List;
+
 @SuppressWarnings("checkstyle:magicnumber")
 public class Mock {
     private List<WorldObject> roadObjects = new ArrayList<>();
+
+    public Mock() {
+        roadObjects.add(new WorldObject(474, 669, "road_2lane_straight.png"));
+        roadObjects.add(new WorldObject(124, 669, "road_2lane_straight.png"));
+        roadObjects.add(new WorldObject(124, 1019, "road_2lane_straight.png"));
+        roadObjects.add(new WorldObject(124, 1369, "road_2lane_straight.png"));
+        roadObjects.add(new WorldObject(124, 1719, "road_2lane_90right.png"));
+
+
+        roadObjects.get(0).setRotation((float) convertMatrixToRadians(1, 0, 0, 1));
+        roadObjects.get(1).setRotation((float) convertMatrixToRadians(1, 0, 0, 1));
+        roadObjects.get(2).setRotation((float) convertMatrixToRadians(1, 0, 0, 1));
+        roadObjects.get(3).setRotation((float) convertMatrixToRadians(1, 0, 0, 1));
+        roadObjects.get(4).setRotation((float) convertMatrixToRadians(1, 0, 0, 1));
+
+
+    }
 
     public static float normalize(float value, float min, float max) {
         return Math.abs((value - min) / (max - min));
@@ -16,34 +35,9 @@ public class Mock {
         return radian > 0 ? radian : 2 * Math.PI + radian;
     }
 
-
-    public Mock() {
-//        roadObjects.add(new WorldObject((int)normalize(300,300,770),(int)normalize(669,0,700),"road_2lane_straight.png"));
-//        roadObjects.add(new WorldObject((int)normalize(300,300,770),(int)normalize(1019,0,700),"road_2lane_straight.png"));
-        roadObjects.add(new WorldObject(474, 669, "road_2lane_straight.png"));
-        roadObjects.add(new WorldObject(124, 669, "road_2lane_straight.png"));
-        roadObjects.add(new WorldObject(124, 1019, "road_2lane_straight.png"));
-        roadObjects.add(new WorldObject(124, 1369, "road_2lane_straight.png"));
-        roadObjects.add(new WorldObject(124, 1719, "road_2lane_90right.png"));
-
-
-        roadObjects.get(0).setRotation((float)convertMatrixToRadians(1, 0, 0, 1));
-        roadObjects.get(1).setRotation((float)convertMatrixToRadians(1, 0, 0, 1));
-        roadObjects.get(2).setRotation((float)convertMatrixToRadians(1, 0, 0, 1));
-        roadObjects.get(3).setRotation((float)convertMatrixToRadians(1, 0, 0, 1));
-        roadObjects.get(4).setRotation((float)convertMatrixToRadians(1, 0, 0, 1));
-
-
-
-
-
-
-    }
-
-    public List<WorldObject> getRoadObjects(){
+    public List<WorldObject> getRoadObjects() {
         return roadObjects;
     }
-
 
 
 }
