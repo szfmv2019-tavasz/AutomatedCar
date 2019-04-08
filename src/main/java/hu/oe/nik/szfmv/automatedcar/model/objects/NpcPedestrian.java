9ab93b53ddf4fdcc1eb091752b231e132783e45a
Róbert <rmegyesi19@gmail.com>
2019-04-08 20:33:09 +0200

    Added Npc classes

---
+++ src/main/java/hu/oe/nik/szfmv/automatedcar/model/objects/NpcPedestrian.java
@@ -0,0 +1,7 @@
+package hu.oe.nik.szfmv.automatedcar.model.objects;
+
+public class NpcPedestrian extends Npc {
+    public NpcPedestrian(int x, int y, String healthyImageFileName, String damagedImageFileName, String deadImageFileName) {
+        super(x, y, healthyImageFileName, damagedImageFileName, deadImageFileName);
+    }
+}
